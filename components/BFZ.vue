ffeb3d9a571c737a01a5fa4a7105afb58d9f9970
thygolem <thygolem@gmail.com>
2021-07-02 12:08:58 +0200

    taskS begins

---
+++ components/BFZ.vue
@@ -0,0 +1,96 @@
+<template>
+<div>
+    <h1> taskS </h1>
+    <h2> Sistemas de automatización de tareas </h2>
+    <h3> Entornos: </h3>
+    <h4> 
+            <ul>Logístico</ul>
+            <ul>Sanitario</ul>
+            <ul>Hostelería</ul>
+    </h4>
+    <hr>
+    <table>
+        <td> NAME <input type="text"> </td>
+        <td> ESPID: <input type="text"> </td>
+        <td> Type <input type="text"> </td>
+        <h3> <button @click="addDevice()"> AÑADIR </button></h3>
+    </table>
+    <hr>
+    <br>
+    <table>
+        <tr>
+            <th> NAME </th>
+            <th> ACTIVE </th>
+            <th> Building </th>
+            <th> Floor </th>
+            <th> Zone </th>
+        </tr>
+        <tr v-for="device in devices" :key="device">
+            <td> UNO </td>
+            <td> {{ device.active }} </td>
+            <td> {{ device.b }} </td>
+            <td> {{ device.f }} </td>
+            <td> {{ device.z }} </td>
+            <td> <button onclick="window.location.href='/test'"> LOCATE {{ device.position }} </button> </td>
+        </tr>
+    </table>
+    <hr>
+</div>
+</template>
+
+<script>
+export default{
+    props: {
+
+    },
+    data() {
+        return {
+            devices:[],
+            newDevice: {
+                b: '',
+                f: '',
+                z: '',
+                active: '',
+                position: '',
+            }, 
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+        this.getDevices();
+    },
+    methods: {
+        addDevice() {
+            //...TO API
+            this.devices.push(JSON.parse(JSON.stringify(this.newDevice)));
+        },
+        getDevices() {
+            //...FROM API
+            var response = [
+                {
+                    b: "A",
+                    f: "B",
+                    z: "C",
+                    active: false,
+                    position: "ESPID:f",
+                },
+                {
+                    b: "X",
+                    f: "Y",
+                    z: "Z",
+                    active: true,
+                    position: "ESPID:a",
+
+                }
+            ];
+            this.devices = response;
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/cv.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+<BFZ>
+</BFZ>
+
+</div>
+</template>
+
+<script>
+export default{
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        
+    },
+}
+</script>
+
+<style>
+    table, th, td {
+        border-collapse: collapse;
+        border: 3px solid red;
+        padding:10px;
+    }
+    h5 {
+        border: 0.5px solid green;
+        padding:10px;
+    }
+    h4 {
+        border: 0.2px solid rgba(228, 134, 12, 0.644);
+        padding:10px;
+    }
+    h3 {
+        border: 0.2px solid rgba(183, 185, 24, 0.747);
+        padding:10px;
+    }
+    h2 {
+        border: 0.2px solid rgba(11, 187, 105, 0.658);
+        padding:10px;
+    }
+    h1 {
+        border: 0.2px solid rgba(0, 119, 255, 0.507);
+        padding:10px;
+    }
+</style>
