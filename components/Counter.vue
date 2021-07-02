b944e8f5711624674e7c43d1ca7f6fa7a54c33de
thygolem <thygolem@gmail.com>
2021-07-02 10:05:18 +0200

    3rd

---
+++ components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+    
+</template>
+
+
+<script>
+    export default{
+        data() {
+            return {
+            }
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+        methods: {
+            
+            },
+    }
+</script>
